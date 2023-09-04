bcc69bfdd4eb6d3ea2df2163997970094564dc5f
Jalil Belakhdar <jalil.belakhdar@trulyexperiences.com>
2023-09-04 15:30:49 +0100

    - Add props for images - Add eslint config file

--- ui-src/App.tsx
+++ ui-src/App.tsx
@@ -4,15 +4,26 @@ import Menu from "./components/Menu";
 import DownloadAllButton from "./components/DownloadAllButton";
 import ImagesList from "./components/ImagesList";
 
+const images = [
+  { name: "image 1", ext: "png", size: "1 mb" },
+  { name: "image 2", ext: "png", size: "1.1 mb" },
+  { name: "image 3", ext: "png", size: "0.1 mb" },
+  { name: "image 4", ext: "png", size: "2.1 mb" },
+  { name: "image 5", ext: "png", size: "0.5 mb" },
+];
+
 function App() {
   return (
     <>
       <Menu />
-      <div className="d-flex flex-column justify-content-start align-items-start w-100 page-container" id="images_container">
-        <ImagesList />
+      <div
+        className="d-flex flex-column justify-content-start align-items-start w-100 page-container"
+        id="images_container"
+      >
+        <ImagesList images={images} />
         <DownloadAllButton />
       </div>
-    </ >
+    </>
   );
 }
 
---
+++ ui-src/entities/Image.ts
@@ -0,0 +1,5 @@
+export default interface Image {
+  name: string;
+  ext: string;
+  size: string;
+}
