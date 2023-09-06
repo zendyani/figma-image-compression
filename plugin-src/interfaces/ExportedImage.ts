8ff41975a480aaaa60728cb6824debc53786c1ff
Jalil Belakhdar <jalil.belakhdar@trulyexperiences.com>
2023-09-06 14:39:46 +0100

    Add suffix Image and add slugify dep

--- plugin-src/interfaces/ExportedImage.ts
+++ plugin-src/interfaces/ExportedImage.ts
@@ -3,4 +3,5 @@ export default interface ExportedImage {
     ext: string;
     image: Uint8Array;
     name: string;
+    suffix?: string;
   }
---
+++ ui-src/services/Download.ts
@@ -0,0 +1,10 @@
+import Image from "../../shared/entities/Image";
+
+const downloadAll = (images: Image[]) => {
+    /**
+     * - Check Image information
+     * - Send to compression service
+     * - Add the result to the zip file
+     * - Force the download operation
+     */
+}
